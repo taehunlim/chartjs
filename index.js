632e973788f21db2fa28909eab975d1bff3d785d
taehunlim <dsa1520@naver.com>
2023-02-19 22:01:56 +0900

    Refactor: add animate function

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ const drawChart = (values) => {
 
   const barWidth = position.max_x / values.length - position.min_x;
 
-  let number = 0;
-
   const writeText = (data, x, y) => {
     const centerPosition = barWidth / 2 - ctx.measureText(data).width / 2;
     ctx.strokeText(data, x + centerPosition, y);
@@ -27,34 +25,42 @@ const drawChart = (values) => {
     ctx.strokeRect(x, y, barWidth, position.max_y - y);
   };
 
-  ctx.beginPath();
+  const animate = () => {
+    let currentHeight = 0;
+    ctx.beginPath();
 
-  const interval = setInterval(() => {
-    number++;
+    return function () {
+      const interval = setInterval(() => {
+        currentHeight++;
 
-    ctx.clearRect(0, 0, width, height);
-    values.forEach((data, idx) => {
-      const divide = idx / values.length;
-      const ratio = 1 - data / 100;
-      const currentX = position.min_x + position.max_x * divide;
-      const currentY = position.max_y * ratio;
+        ctx.clearRect(0, 0, width, height);
+        values.forEach((data, idx) => {
+          const divide = idx / values.length;
+          const ratio = 1 - data / 100;
+          const currentX = position.min_x + position.max_x * divide;
+          const currentY = position.max_y * ratio;
 
-      if (data > number) {
-        const ratio = 1 - number / 100;
-        const currentY = position.max_y * ratio;
+          if (data > currentHeight) {
+            const ratio = 1 - currentHeight / 100;
+            const currentY = position.max_y * ratio;
 
-        return drawBar(currentX, currentY);
-      }
+            return drawBar(currentX, currentY);
+          }
 
-      drawBar(currentX, currentY);
+          drawBar(currentX, currentY);
 
-      if (number >= Math.max(...values)) {
-        writeText(data, currentX, position.max_y + 20);
+          if (currentHeight >= Math.max(...values)) {
+            writeText(data, currentX, position.max_y + 20);
 
-        return clearInterval(interval);
-      }
-    });
-  }, 10);
+            return clearInterval(interval);
+          }
+        });
+      }, 10);
+    };
+  };
+
+  const draw = animate();
+  draw();
 };
 
 drawChart(values);
