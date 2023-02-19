e6d9315f6e3618fb5a14896a7ea00169b5f30fd6
taehunlim <dsa1520@naver.com>
2023-02-19 21:17:23 +0900

    Feat: create text for the value of chart

--- index.js
+++ index.js
@@ -1,46 +1,65 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const drawChart = () => {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
 
-let width = canvas.clientWidth;
-let height = canvas.clientHeight;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
 
-let position = {
-  min_x: width * 0.1,
-  max_x: width * 0.9,
-  min_y: height * 0.1,
-  max_y: height * 0.9,
+  const position = {
+    min_x: width * 0.1,
+    max_x: width * 0.9,
+    min_y: height * 0.1,
+    max_y: height * 0.9,
+  };
+
+  const values = [90, 50, 40, 90, 100];
+  const wid = position.max_x / values.length - position.min_x;
+
+  let number = 0;
+
+  const writeText = (data, x, y) => {
+    const centerPosition = wid / 2 - ctx.measureText(data).width / 2;
+
+    ctx.strokeText(data, x + centerPosition, y);
+  };
+
+  ctx.beginPath();
+
+  const interval = setInterval(() => {
+    number++;
+
+    ctx.clearRect(0, 0, width, height);
+    values.forEach((data, idx) => {
+      const divide = idx / values.length;
+      const ratio = 1 - number / 100;
+
+      if (data > number) {
+        return ctx.strokeRect(
+          position.min_x + position.max_x * divide,
+          position.max_y * ratio,
+          wid,
+          position.max_y - position.max_y * ratio
+        );
+      }
+
+      ctx.strokeRect(
+        position.min_x + position.max_x * divide,
+        position.max_y * (1 - data / 100),
+        wid,
+        position.max_y - position.max_y * (1 - data / 100)
+      );
+
+      if (number >= Math.max(...values)) {
+        writeText(
+          data,
+          position.min_x + position.max_x * divide,
+          position.max_y + 20
+        );
+
+        return clearInterval(interval);
+      }
+    });
+  }, 10);
 };
 
-let values = [90, 50, 40, 90, 100, 20, 10];
-let wid = position.max_x / values.length - position.min_x;
-
-ctx.beginPath();
-
-let number = 0;
-let interval = setInterval(() => {
-  number++;
-
-  ctx.clearRect(0, 0, width, height);
-  values.forEach((data, idx) => {
-    let divide = idx / values.length;
-    let ratio = 1 - number / 100;
-
-    if (data > number) {
-      return ctx.strokeRect(
-        position.min_x + position.max_x * divide,
-        position.max_y * ratio,
-        wid,
-        position.max_y - position.max_y * ratio
-      );
-    }
-
-    ctx.strokeRect(
-      position.min_x + position.max_x * divide,
-      position.max_y * (1 - data / 100),
-      wid,
-      position.max_y - position.max_y * (1 - data / 100)
-    );
-
-    if (number >= Math.max(...values)) clearInterval(interval);
-  });
-}, 10);
+drawChart();
